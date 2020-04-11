ee7fef400467267ed393c6f44c9b27d252fd895a
PABLO ISRAEL <gperfar@gmail.com>
2020-04-11 12:14:18 -0300

    Making everything better

--- api/models/Sentence.py
+++ api/models/Sentence.py
@@ -6,14 +6,16 @@ class Sentence(Mixin, db.Model):
     """Sentence Table."""
     __tablename__ = "sentence"
     id = db.Column(db.Integer, unique=True, primary_key=True)
-    sentence = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    sql_query = db.Column(db.String, nullable=False)
     connection = db.Column(db.Integer, db.ForeignKey("connection.id", ondelete="CASCADE"), nullable=False)
     comment = db.Column(db.String, nullable=True)
 
-    def __init__(self, sentence:str, connection:int, comment:str  ):
-        self.sentence = sentence
+    def __init__(self, name:str, sql_query:str, connection:int, comment:str  ):
+        self.name = name
+        self.sql_query = sql_query
         self.connection = connection
         self.comment = comment
 
     def __repr__(self):
-        return f"<Sentence {self.sentence}>"
+        return f"<Sentence {self.name}>"
--- api/views/main.py
+++ api/views/main.py
@@ -6,20 +6,15 @@ import psycopg2, json
 
 main = Blueprint("main", __name__)  # initialize blueprint
 
-
-def start_db_connection(connection_id):
+def start_db_connection(connection):
     conn = None
-    connection_host = Connection.query.get(connection_id).host
-    connection_database = Connection.query.get(connection_id).database
-    connection_username = Connection.query.get(connection_id).username
-    connection_password = Connection.query.get(connection_id).password
     try:
         #Connect to the DB	
         conn = psycopg2.connect(
-            host = connection_host,
-            database = connection_database, 
-            user = connection_username, 
-            password = connection_password)
+            host = connection.host,
+            database = connection.database, 
+            user = connection.username, 
+            password = connection.password)
         return conn
     except (Exception, psycopg2.DatabaseError) as error:
         return(error)
@@ -33,47 +28,18 @@ def end_db_connection(cur, conn):
     finally:
         return True
     
-def execute_sentence(connection_id, sentence_id):
-    if Sentence.query.get(sentence_id).connection == int(connection_id):
-        conn = start_db_connection(connection_id)
+def execute_sentence(sentence):
+    try:
+        connection = Connection.query.get(sentence.connection)
+        conn = start_db_connection(connection)
         cur = conn.cursor()
-        sentence = Sentence.query.get(sentence_id).sentence
-        cur.execute(sentence)
+        cur.execute(sentence.sql_query)
         results= cur.fetchall()
         results_json = [dict(zip([key[0] for key in cur.description], row)) for row in results] #Googleada, ni idea cómo funciona pero it does
         end_db_connection(cur,conn)
         return results_json
-    else:
-        return json.dumps({
-            'status': "error",
-            'message': "Security error: sentence connection is not the given connection"
-            })
-
-@main.route('/testconnection', methods=['GET']) #PROBAR SIEMPRE CON LA CONNECTION DE ID 3 Y SENTENCE DE ID 2
-def selecttables():
-    query_params = request.args
-    connection_id = query_params.get('connection_id')
-    sentence_id = query_params.get('sentence_id')
-    if (not(connection_id) or not(sentence_id)):
-        return "Missing connection_id or sentence_id" 
-    if Sentence.query.get(sentence_id).connection == int(connection_id):
-        results_json = execute_sentence(connection_id, sentence_id)
-        connection_name = Connection.query.get(connection_id).name
-        sentence = Sentence.query.get(sentence_id).sentence
-        sentence_connection = Sentence.query.get(sentence_id).connection
-        return json.dumps(
-            {'connection_id':connection_id,
-            'connection name': connection_name,
-            'sentence': sentence,
-            'sentence connection': sentence_connection,
-            'security OK': "Yes" if sentence_connection == int(connection_id) else "No",
-            'results': results_json
-            })
-    else: 
-        return json.dumps({
-            'status': "error",
-            'message': "Security error: sentence connection is not the given connection"
-            })
+    except(Exception) as error:
+        return json.dumps({'status': "error",'message': error})
 
 # function that is called when you visit /
 @main.route("/")
@@ -84,46 +50,74 @@ def index():
     logger.info("Hello, Ring Bearer")
     return "<h1>Hello, Ring Bearer</h1>"
 
+@main.app_errorhandler(404)
+def page_not_found(e):
+    return "<h1>404: Not found</h1><p>\" I have no memory of this place\".</p><p>Gandalf the Grey</p> <img src=\"https://media1.tenor.com/images/034e7f9bd0df198f758dad87858b0004/tenor.gif?itemid=9228837\">", 404
+    
+@main.route('/runquery', methods=['POST'])
+def runquery():
+    data = request.get_json()
+    logger.info("Data recieved: %s", data)
+    if "connection_id" not in data:
+        msg = "You can't run a query if you don't specify which connection to use."
+        logger.info(msg)
+        return create_response(status=422, message=msg)
+    if "query" not in data:
+        msg = "You can't run a query if you don't give me the query..."
+        logger.info(msg)
+        return create_response(status=422, message=msg)
+    sentence = Sentence(name="Temporary sentence", sql_query=data["query"], connection = data["connection_id"], comment = "Temporary sentence")
+    connection = Connection.query.get(data["connection_id"])
+    #return json.dumps({'sentence': sentence.sql_query, 'connection': connection.name})
+    try:
+        results_json = execute_sentence(sentence)
+        return json.dumps({'connection_id':connection.id,'connection name': connection.name,'sentence': sentence.sql_query,'results': results_json})
+    except(Exception) as error:
+        create_response(status=500,message= error)
+
+@main.route('/runquery', methods=['GET']) #PROBAR SIEMPRE CON LA CONNECTION DE ID 3 Y SENTENCE DE ID 2
+def runquery_get():
+    query_params = request.args
+    sentence_id = query_params.get('sentence_id')
+    if (not(sentence_id)):
+        msg = "Missing sentence_id" 
+        return create_response(status=422, message=msg)
+    sentence = Sentence.query.get(sentence_id)
+    connection = Connection.query.get(sentence.connection)
+    try:
+        results_json = execute_sentence(sentence)
+        return json.dumps({'connection name': connection.name,'sentence': sentence.sql_query,'results': results_json})
+    except(Exception) as error:
+        create_response({'status': 422,'message': error})
+
 # function that is called when you visit /persons
 @main.route("/init", methods=["GET"])
 def init_data():
     #Users
     dummyusers =[]
-    user1 = User(name="Aragorn", email="AragornStrider@gondor.com", password="KingOfGondor123")
-    user2 = User(name="Gandalf", email="WhiteWizard@gondor.com", password="FlyYouFools!")
-    dummyusers.append(user1)
-    dummyusers.append(user2)
+    dummyusers.append(User(name="Aragorn", email="AragornStrider@gondor.com", password="KingOfGondor123"))
+    dummyusers.append(User(name="Gandalf", email="WhiteWizard@gondor.com", password="FlyYouFools!"))
     db.session.add_all(dummyusers)
     db.session.commit()
     #Connections
     dummyconnections =[]
-    connection1 = Connection(
-        name="Aragorn's Connection 1", host="AragornStrider.gondor.com", database="Gondor", username = "userr", password = "password", comment = "",user = 1)
-    connection2 = Connection(
-        name="Aragorn's Connection 2", host="AragornStrider2.gondor.com", database="Gondor2", username = "userr2", password = "password2",comment = "", user = 1)
-    connection3 = Connection(
-        name = "Real Connection", host = "drona.db.elephantsql.com", database = "uvqhwsnn", username = "uvqhwsnn", password = "mzwjhs6qcqZHTm-ecCXJkQ3FoLViB9RT", comment = "Conexión real! Base Northwind", user = 2
-    )
+    connection1 = Connection(name="Aragorn's Connection 1", host="AragornStrider.gondor.com", database="Gondor", username = "userr", password = "password", comment = "",user = 1)
+    connection2 = Connection(name="Aragorn's Connection 2", host="AragornStrider2.gondor.com", database="Gondor2", username = "userr2", password = "password2",comment = "", user = 1)
+    connection3 = Connection(name = "Real Connection", host = "drona.db.elephantsql.com", database = "uvqhwsnn", username = "uvqhwsnn", password = "mzwjhs6qcqZHTm-ecCXJkQ3FoLViB9RT", comment = "Conexión real! Base Northwind", user = 2)
     dummyconnections.append(connection1)
     dummyconnections.append(connection2)
     dummyconnections.append(connection3)
     db.session.add_all(dummyconnections)
     db.session.commit()
     #Sentences
-    sentence1 = Sentence(sentence="SELECT * FROM people WHERE kingdom = 'Rohan'", connection = 1, comment = "Keeping tabs on the Rohirrim")
-    sentence2 = Sentence(sentence="SELECT * FROM Customers", connection = 3, comment = "Keeping tabs on Customers") 
+    sentence1 = Sentence(name="Sentence 1", sql_query="SELECT * FROM people WHERE kingdom = 'Rohan'", connection = 1, comment = "Keeping tabs on the Rohirrim")
+    sentence2 = Sentence(name="Sentence 2", sql_query="SELECT * FROM Customers", connection = 3, comment = "Keeping tabs on Customers") 
     db.session.add(sentence1)
     db.session.add(sentence2)
     db.session.commit()
+    msg = "Data has been initialized :D"
+    return create_response(status=200, message=msg)
 
-    return json.dumps({
-            'status': "OK",
-            'message': "Data has been initialized :D"
-            })
-
-@main.app_errorhandler(404)
-def page_not_found(e):
-    return "<h1>404: Not found</h1><p>\" I have no memory of this place\".</p><p>Gandalf the Grey</p> <img src=\"https://media1.tenor.com/images/034e7f9bd0df198f758dad87858b0004/tenor.gif?itemid=9228837\">", 404
 
 # function that is called when you visit /connections
 @main.route("/connections", methods=["GET"])
@@ -162,9 +156,6 @@ def create_connection():
         return create_response(status=422, message=msg)
     if "comment" not in data:
         data["comment"] = ""
-
-    #AGREGAR EL RESTO DE LOS FIELDS (REQUIRED)
-
     # create SQLAlchemy Object
     new_connection = Connection(name=data["name"], host = data["host"], database = data["database"], username = data["username"], password = data["password"], user = data["user_id"], comment = data["comment"])
     # commit it to database
@@ -219,8 +210,12 @@ def get_sentences():
 def create_sentence():
     data = request.get_json()
     logger.info("Data recieved: %s", data)
-    if "sentence" not in data:
-        msg = "No sentence provided for sentence."
+    if "name" not in data:
+        msg = "No name provided for sentence."
+        logger.info(msg)
+        return create_response(status=422, message=msg)
+    if "sql_query" not in data:
+        msg = "No SQL query provided for sentence."
         logger.info(msg)
         return create_response(status=422, message=msg)
     if "connection_id" not in data:
@@ -230,10 +225,10 @@ def create_sentence():
     if "comment" not in data:
         data["comment"] = ""
     # create SQLAlchemy Object
-    new_sentence = Sentence(sentence=data["sentence"], connection = data["connection_id"], comment = data["comment"])
+    new_sentence = Sentence(name = data["name"], sql_query=data["sql_query"], connection = data["connection_id"], comment = data["comment"])
     # commit it to database
     db.session.add(new_sentence)
     db.session.commit()
     return create_response(
-        message=f"Successfully created sentence {new_sentence.sentence} with id: {new_sentence.id}"
+        message=f"Successfully created sentence {new_sentence.name} with id: {new_sentence.id}"
     )
